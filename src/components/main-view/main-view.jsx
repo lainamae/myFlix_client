c4e808eb527921d74ad75af417c07a3a8fdff698
lainamae <64325945+lainamae@users.noreply.github.com>
2021-09-29 16:15:01 -0400

    added image paths and imported jpgs

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import img300 from '../../img/300.jpg';
+import imgShawshank from '../../img/shawshankredemption.jpg';
+import imgInception from '../../img/inception.jpg';
+
 
 export default class MainView extends React.Component {
   constructor(){
     super();
     this.state = {
       movies: [
-        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: 'https://flxt.tmsimg.com/assets/p7825626_p_v10_af.jpg'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/9964546b0ba1f6e14a6045e34b341f8ca2a3569752c5afed95b89682fcde1a68._RI_V_TTW_.jpg'},
-        { _id: 3, Title: '300', Description: 'desc3...', ImagePath: 'https://flxt.tmsimg.com/assets/p163191_p_v10_an.jpg'}
+        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: imgInception },
+        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: imgShawshank},
+        { _id: 3, Title: '300', Description: 'desc3...', ImagePath: img300}
       ],
       selectedMovie: null
     };
