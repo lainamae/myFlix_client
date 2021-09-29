406fe9d01e528af3b1e2d35d2428babe8ec666d3
lainamae <64325945+lainamae@users.noreply.github.com>
2021-09-29 12:17:42 -0400

    added img urls

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,9 +7,9 @@ export default class MainView extends React.Component {
     super();
     this.state = {
       movies: [
-        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-        { _id: 3, Title: '300', Description: 'desc3...', ImagePath: '...'}
+        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: 'https://flxt.tmsimg.com/assets/p7825626_p_v10_af.jpg'},
+        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/9964546b0ba1f6e14a6045e34b341f8ca2a3569752c5afed95b89682fcde1a68._RI_V_TTW_.jpg'},
+        { _id: 3, Title: '300', Description: 'desc3...', ImagePath: 'https://flxt.tmsimg.com/assets/p163191_p_v10_an.jpg'}
       ],
       selectedMovie: null
     };
