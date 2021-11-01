184b8ba44ca73c4be5c99b21ef479826f01b21e6
lainamae <64325945+lainamae@users.noreply.github.com>
2021-11-01 13:12:44 -0400

    update movie-view card format and styling

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MovieImg from '../../img/placeholder.png';
 import Button from 'react-bootstrap/Button';
 
+import './movie-view.scss';
+
+
 import { Link } from "react-router-dom";
+import { Card } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
 
@@ -12,28 +15,25 @@ export class MovieView extends React.Component {
 
     return (
       <div className="movie-view">
-        <div className="movie-poster">
-          <img src={MovieImg} width="100%" height="250px" />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.name}</span>
-        </div>
-        <Link to={`/directors/${movie.Director.Name}`}>
-          <Button variant="link">Director</Button>
-        </Link>
-        <Link to={`/genres/${movie.Genre.Name}`}>
-          <Button variant="link">Genre</Button>
-        </Link>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
+        <Card className="text-white movieview-card">
+          <Card.Img src={movie.MovieStill} crossOrigin="anonymous" />
+          <div className="card-img-overlay d-flex flex-column justify-content-between">
+            <span className="movie-info">
+              <Card.Title><h2>{movie.Title}</h2></Card.Title>
+              <Card.Text>{movie.Description}</Card.Text>
+              <span className="d-block">
+                <Link to={`/directors/${movie.Director.Name}`}>
+                  <Button variant="secondary">Director: {movie.Director.Name}</Button>
+                </Link>
+                <Link to={`/genres/${movie.Genre.Name}`}>
+                  <Button variant="secondary">Genre: {movie.Genre.Name}</Button>
+                </Link>
+              </span>
+            </span>
+
+            <Button variant="outline-light" className="back-btn" onClick={() => { onBackClick(null); }}>Back</Button>
+          </div>
+        </Card>
       </div>
     );
   }
