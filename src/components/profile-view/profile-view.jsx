b5998e42e5cc4faa71fb2bc49f53736971dadb9e
lainamae <64325945+lainamae@users.noreply.github.com>
2021-11-02 13:41:32 -0400

    implement update user form and favorites functionality

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -137,13 +137,69 @@ export class ProfileView extends React.Component {
   render() {
     const { username, email, birthday, favoriteMovies } = this.state;
     const { movies } = this.props;
+    const FavoriteMovies = favoriteMovies;
+    console.log(favoriteMovies);
     return (
-      <Row>
-        <Col>
-          <h1>hello {user}</h1>
-          <Button variant="button" onClick={() => { this.onLoggedOut() }}>Logout</Button>
-        </Col>
-      </Row>
+      <Card className="text-white">
+        <Card.Header><h1>Hello {username}!</h1></Card.Header>
+        <Card.Header><h2>Account information</h2></Card.Header>
+        <Card.Body><p>USERNAME: {username}</p>
+          <p>EMAIL: {email}</p>
+          <p>BIRTHDAY: {birthday}</p></Card.Body>
+        <Accordion>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            <span className="d-flex flex-row justify-content-between align-items-center"><h2>Update account information</h2> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-chevron-expand" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M3.646 9.146a.5.5 0 0 1 .708 0L8 12.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708zm0-2.292a.5.5 0 0 0 .708 0L8 3.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708z" />
+            </svg></span>
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <Form className="formDisplay" onSubmit={(e) => this.editUser(e)}>
+                <Form.Group>
+                  <Form.Group>
+                    Username
+                    <Form.Control type='text' name="Username" placeholder="New Username" onChange={(e) => this.setUsername(e.target.value)} />
+                  </Form.Group>
+                  <Form.Group>
+                    Password
+                    <Form.Control type='password' name="Password" placeholder="New Password" onChange={(e) => this.setPassword(e.target.value)} />
+
+                  </Form.Group>
+                  <Form.Group>
+                    Email Address
+                    <Form.Control type='email' name="Email" placeholder="New Email" onChange={(e) => this.setEmail(e.target.value)} />
+
+                  </Form.Group>
+                  <Form.Group>
+                    Birthday
+                    <Form.Control type='date' name="Birthday" onChange={(e) => this.setBirthday(e.target.value)} />
+
+                  </Form.Group>
+                </Form.Group>
+                <Button variant="success" type="submit" >Update</Button>
+              </Form>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Accordion>
+        <Card.Header><h2> Favorites</h2></Card.Header>
+        <div className="d-flex flew-column">
+          {
+            FavoriteMovies.map((movie) => (
+              <Card key={movie._id}>
+                <Card.Img variant="top" src={movie.ImagePath} crossOrigin="anonymous" />
+                <Card.Body>
+                  <Card.Title>{movie.Title}</Card.Title>
+                  <Card.Text><Button variant="outline-danger" size="sm" onClick={() => { this.removeFavorite(movie._id) }}>Remove favorite</Button></Card.Text>
+                </Card.Body>
+              </Card>
+            ))
+          }
+        </div>
+        <Card.Body>
+          <Button variant="dark" onClick={() => { this.onLoggedOut() }}>Logout</Button>
+          <Button variant="danger" onClick={() => { this.deleteAccount() }}>Delete Account</Button>
+        </Card.Body>
+      </Card>
     )
   }
 }
