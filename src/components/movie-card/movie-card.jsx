1bb05ffe9064b6f6f24350bf7f7e0b1bbe30280e
lainamae <64325945+lainamae@users.noreply.github.com>
2021-11-05 19:38:08 -0400

    styling update

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-
 import './movie-card.scss';
 
-import { Link } from "react-router-dom";
+const heartIcon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+	<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+</svg>
 
 export class MovieCard extends React.Component {
 	render() {
-		const { movie, onMovieClick } = this.props;
+		const { movie, addFavorite } = this.props;
 		console.log(movie);
 		return (
-			<Card className="bg-dark text-white align-self-stretch">
+			<Card className="movie-card">
 				<Card.Img variant="top" src={movie.ImagePath} crossOrigin="anonymous" />
-				<Card.Body className="d-flex flex-column">
-					<Card.Title>{movie.Title}</Card.Title>
-					<div className="mt-auto d-grid gap-2">
-						<Link to={`/movies/${movie._id}`}>
-							<Button variant="outline-primary" className="movie-btn">Open</Button>
-						</Link>
-					</div>
-				</Card.Body>
+				<Link to={`/movies/${movie._id}`}>
+					<Card.ImgOverlay className="movie-overlay">
+						<h2 className="card-title">{movie.Title}</h2>
+						<button onClick={() => { addFavorite(movie._id) }}>{heartIcon}</button>
+					</Card.ImgOverlay>
+				</Link>
+				{/* <div className="movie-card-btns">
+					<Button variant="outline-danger" size="sm" onClick={() => { addFavorite(movie._id) }}>{heartIcon}</Button>
+
+				</div> */}
 			</Card>
 		);
 	}
